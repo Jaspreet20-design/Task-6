abfd4cc3f3f26336e52b004ab75c6c30c3d7d56f
Jaspreet20-design <79186216+Jaspreet20-design@users.noreply.github.com>
2021-03-15 18:35:00 +0530

    Task 6

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
